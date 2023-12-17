2104ea672ee23f8c7f9d3c81fd97d611f31ff59d
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2023-12-17 03:34:16 +0300

    abc

---
+++ Services/Services/CommandService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Entity.IUnitOfWork;
+using Entity.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class CommandService : ICommandService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public CommandService(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+    }
+}
