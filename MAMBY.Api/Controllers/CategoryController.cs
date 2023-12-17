0ad0e3312a096a6fab7afec6478147947e111127
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2023-12-17 17:30:27 +0300

    category finish

---
+++ MAMBY.Api/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Entity.DTOs;
+using Entity.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MAMBY.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet("GetAllCategory")]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            var categories = await _categoryService.GetAllCategory();
+            return Ok(categories);
+        }
+
+        [HttpGet("DeleteCategory")]
+        public IActionResult DeleteCategory(int id)
+        {
+            _categoryService.DeleteCategory(id);
+            return Ok();
+        }
+        [HttpPost("CreateCategory")]
+        public async Task<IActionResult> CreateCategory([FromBody] CategoryDto categoryDto)
+        {
+            var category = await _categoryService.CreateCategory(categoryDto);
+            return Ok(category);
+        }
+        [HttpPut("UpdateCategory")]
+        public async Task<IActionResult> UpdateCategory([FromBody] CategoryDto categoryDto)
+        {
+            var category = await _categoryService.UpdateCategory(categoryDto);
+            return Ok(category);
+        }
+    }
+}
